138823d2a88436c9a9fd0ae8c85358e2fc1de5b9
Thanadon Tachawatchareekun <thanadon.ta@inet.co.th>
2019-06-16 19:59:47 +0700

    many zone

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ CORS(app, resources={r"/*": {"origins": "*"}} )
 app.config['MYSQL_DATABASE_USER'] = 'smart'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'P@ssword'
 app.config['MYSQL_DATABASE_DB'] = 'cp_warehouse'
-# app.config['MYSQL_DATABASE_HOST'] = '35.186.149.130'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = '35.186.149.130'
+# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 
 
 mysql = MySQL()
@@ -92,22 +92,38 @@ def getZone():
     except Exception as e:
         return jsonify({"status": "fail","message":str(e)})
 
+
+@app.route('/getnumzone', methods=['GET'])
+def getnumzone():
+    try:
+        result = querySelect_DB("SELECT * FROM zone_info")
+
+        return jsonify({"status": "success","num":len(result)})
+    except Exception as e:
+        return jsonify({"status": "fail","message":str(e)})
+
 @app.route('/realtimeusage', methods=['POST'])
 def realtimeusage():
     try:
         data = request.json
         ZoneID = data['ZoneID']
         result = querySelect_DB("SELECT ZoneID,RT_PSum, AVG((RT_V12+RT_V23+RT_V31)/3) as RT_VSum, AVG((RT_I1+RT_I2+RT_I3)/3) as RT_ISum FROM meter_info WHERE ZoneID = '"+ZoneID+"' AND MeterID = '1' GROUP BY ZoneID")
-       
-        RT_PSum = result[0]['RT_PSum']
-        RT_ISum = result[0]['RT_ISum']
-        RT_VSum = result[0]['RT_VSum']
+        # return str(result)
+        if result == [] or result == False:
+            RT_PSum = "0"
+            RT_ISum = "0"
+            RT_VSum = "0"
 
-        if str(RT_ISum) != 'None':
-            RT_ISum = "%.2f" % RT_ISum
+        else:
+            RT_PSum = result[0]['RT_PSum']
+            RT_ISum = result[0]['RT_ISum']
+            RT_VSum = result[0]['RT_VSum']
 
-        if str(RT_ISum) != 'None':
-            RT_VSum = "%.2f" % RT_VSum
+            if str(RT_ISum) != 'None':
+                RT_ISum = "%.2f" % RT_ISum
+
+            if str(RT_ISum) != 'None':
+                RT_VSum = "%.2f" % RT_VSum
 
         return jsonify({"status": "success","RT_PSum":str(RT_PSum),"RT_ISum":str(RT_ISum),"RT_VSum":str(RT_VSum)})
     except Exception as e:
@@ -119,7 +135,7 @@ def getdatart():
         data = request.json
         ZoneID = data['ZoneID']
         # ZoneID = '1'
-        result = querySelect_DB("SELECT RT_kWh_Today,RT_kWh_Daily_Avg,RT_kWh_Monthly,RT_kWh_Monthly_Avg FROM meter_info WHERE ZoneID = '"+ZoneID+"' ")
+        result = querySelect_DB("SELECT RT_kWh_Today,RT_kWh_Daily_Avg,RT_kWh_Monthly,RT_kWh_Monthly_Avg FROM meter_info WHERE ZoneID = '"+ZoneID+"'  ")
         if result == [] or result == False:
             return jsonify({"status": "fail","message":"not found"})
         # return str(result)
@@ -127,6 +143,31 @@ def getdatart():
         RT_kWh_Daily_Avg = str(int(result[0]['RT_kWh_Daily_Avg']))
         RT_kWh_Monthly = str(int(result[0]['RT_kWh_Monthly']))
         RT_kWh_Monthly_Avg = str(int(result[0]['RT_kWh_Monthly_Avg']))
+
+        return jsonify({"status": "success","RT_kWh_Today":RT_kWh_Today,"RT_kWh_Daily_Avg":RT_kWh_Daily_Avg,"RT_kWh_Monthly":RT_kWh_Monthly,"RT_kWh_Monthly_Avg":RT_kWh_Monthly_Avg})
+    except Exception as e:
+        return jsonify({"status": "fail","message":str(e)})
+
+@app.route('/getdatart2', methods=['POST'])
+def getdatart2():
+    try:
+        data = request.json
+
+        ZoneID = data['ZoneID']
+        list_RT = []
+        result_meter = querySelect_DB("SELECT MeterID,MeterName FROM meter_info WHERE ZoneID = '"+str(ZoneID)+"'")
+
+        for i in result_meter:
+            result = querySelect_DB("SELECT RT_kWh_Today,RT_kWh_Daily_Avg,RT_kWh_Monthly,RT_kWh_Monthly_Avg FROM meter_info WHERE ZoneID = '"+i['ZoneID']+"'  ")
+            if result == [] or result == False:
+                return jsonify({"status": "fail","message":"not found"})
+            # return str(result)
+            RT_kWh_Today = str(int(result[0]['RT_kWh_Today']))
+            RT_kWh_Daily_Avg = str(int(result[0]['RT_kWh_Daily_Avg']))
+            RT_kWh_Monthly = str(int(result[0]['RT_kWh_Monthly']))
+            RT_kWh_Monthly_Avg = str(int(result[0]['RT_kWh_Monthly_Avg']))
+
+            list_RT.append({"RT_kWh_Today":RT_kWh_Today,"RT_kWh_Daily_Avg":RT_kWh_Daily_Avg,"RT_kWh_Monthly":RT_kWh_Monthly,"RT_kWh_Monthly_Avg":RT_kWh_Monthly_Avg})
 
         return jsonify({"status": "success","RT_kWh_Today":RT_kWh_Today,"RT_kWh_Daily_Avg":RT_kWh_Daily_Avg,"RT_kWh_Monthly":RT_kWh_Monthly,"RT_kWh_Monthly_Avg":RT_kWh_Monthly_Avg})
     except Exception as e:
@@ -198,6 +239,8 @@ def logpsum2():
         data = request.json
         ZoneID = data['ZoneID']
         list_psum = []
+        date_Psum = []
+        Psum = []
         result_meter = querySelect_DB("SELECT MeterID,MeterName FROM meter_info WHERE ZoneID = '"+str(ZoneID)+"'")
         # return  str(result_meter)
         for i in result_meter:
@@ -231,12 +274,148 @@ def logpsum2():
         return jsonify({"status": "success","list_psum":list_psum,"date_Psum":date_Psum})
     
     except Exception as e:
+        return jsonify({"status": "fail"})
+
+@app.route('/datameter', methods=['POST'])
+def datameter():
+    try:
+        data = request.json
+
+        ZoneID = data['ZoneID']
+        list_data = []
+        result_meter = querySelect_DB("SELECT * FROM meter_info WHERE ZoneID = '"+str(ZoneID)+"'")
+        if result_meter == [] or result_meter == False:
+            return jsonify({"status": "fail","message":"not found"})
+        # result_meter = result_meter[0]
+
+
+        for i in result_meter:
+            list_data.append(
+                {
+                "MeterName":str(i['MeterName']),
+                "RT_V1":str(i['RT_V1']),
+                "RT_V2":str(i['RT_V2']),
+                "RT_V3":str(i['RT_V3']),
+                "RT_V12":str(i['RT_V12']),
+                "RT_V23":str(i['RT_V23']),
+                "RT_V31":str(i['RT_V31']),
+                "RT_I1":str(i['RT_I1']),
+                "RT_I2":str(i['RT_I2']),
+                "RT_I3":str(i['RT_I3']),
+                "RT_In":str(i['RT_In']),
+                "RT_Pa":str(i['RT_Pa']),
+                "RT_Pb":str(i['RT_Pb']),
+                "RT_Pc":str(i['RT_Pc']),
+                "RT_PSum":str(i['RT_PSum']),
+                "RT_Qa":str(i['RT_Qa']),
+                "RT_Qb":str(i['RT_Qb']),
+                "RT_Qc":str(i['RT_Qc']),
+                "RT_PFa":str(i['RT_PFa']),
+                "RT_PFb":str(i['RT_PFb']),
+                "RT_PFc":str(i['RT_PFc']),
+                "RT_PFSum":str(i['RT_PFSum']),
+                "RT_Sa":str(i['RT_Sa']),
+                "RT_Sb":str(i['RT_Sb']),
+                "RT_Sc":str(i['RT_Sc']),
+                "RT_F":str(i['RT_F']),
+                "RT_kWh":str(i['RT_kWh']),
+                })
+        for i in result_meter:
+            list_data.append(
+                {
+                "MeterName":str(i['MeterName']),
+                "RT_V1":str(i['RT_V1']),
+                "RT_V2":str(i['RT_V2']),
+                "RT_V3":str(i['RT_V3']),
+                "RT_V12":str(i['RT_V12']),
+                "RT_V23":str(i['RT_V23']),
+                "RT_V31":str(i['RT_V31']),
+                "RT_I1":str(i['RT_I1']),
+                "RT_I2":str(i['RT_I2']),
+                "RT_I3":str(i['RT_I3']),
+                "RT_In":str(i['RT_In']),
+                "RT_Pa":str(i['RT_Pa']),
+                "RT_Pb":str(i['RT_Pb']),
+                "RT_Pc":str(i['RT_Pc']),
+                "RT_PSum":str(i['RT_PSum']),
+                "RT_Qa":str(i['RT_Qa']),
+                "RT_Qb":str(i['RT_Qb']),
+                "RT_Qc":str(i['RT_Qc']),
+                "RT_PFa":str(i['RT_PFa']),
+                "RT_PFb":str(i['RT_PFb']),
+                "RT_PFc":str(i['RT_PFc']),
+                "RT_PFSum":str(i['RT_PFSum']),
+                "RT_Sa":str(i['RT_Sa']),
+                "RT_Sb":str(i['RT_Sb']),
+                "RT_Sc":str(i['RT_Sc']),
+                "RT_F":str(i['RT_F']),
+                "RT_kWh":str(i['RT_kWh']),
+                })
+
+
+        return jsonify({"status": "success","datameter":list_data})
+    
+    except Exception as e:
+        return jsonify({"status": "fail","messgae":str(e)})
+
+@app.route('/logpsumall', methods=['POST'])
+def logpsumall():
+    try:
+        data = request.json
+
+        SiteID = data['SiteID']
+        list_psum = []
+        date_Psum = []
+        Psum = []
+        # return password
+        diff = []
+        now = datetime.datetime.now()
+        time_start = str(now.date())+ " 00:00:00"
+        time_stop = str(now.date())+ " 23:59:59"
+
+        result_site = querySelect_DB("SELECT * FROM zone_info WHERE SiteID = '"+str(SiteID)+"'")
+       
+        for s in result_site:
+        
+            result_meter = querySelect_DB("SELECT MeterID,MeterName FROM meter_info WHERE ZoneID = '"+str(s['ZoneID'])+"'")
+            # return  str(result_meter)
+            for i in result_meter:
+                result_log = querySelect_DB("SELECT Hour(log_Date) HourInDay,sum(Log_kWh_Diff) as diff FROM cp_warehouse.meter_log where day(log_Date) = day(CURDATE())  and MeterID = '"+str(i['MeterID'])+"' group by HourInDay order by HourInDay")
+                # result = querySelect_DB("SELECT Log_Date,Log_PSum FROM meter_log WHERE MeterID = '"+MeterID+"' LIMIT 8")
+                # print str(result)
+                if result_log == [] or result_log == False:
+                    return jsonify({"status": "fail","message":"not found"})
+                # return str(result)
+                date_Psum = ["0","1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20","21","22","23"]
+                Psum = []
+                
+                for d in date_Psum:
+                    Psum.append("0")
+
+                # for k in date_Psum:
+                for j in result_log:
+                     Psum[j['HourInDay']] = str(j['diff'])
+
+
+                name = "ZoneID " +str(s['ZoneID'])+" ("+i['MeterName']+")" 
+
+                list_psum.append({"name":str(name),"data":Psum })
+
+        return jsonify({"status": "success","list_psum":list_psum,"hour_Psum":date_Psum})
+    
+    except Exception as e:
         return jsonify({"status": "fail","message":str(e)})
+
+
 
 @app.route('/sumday', methods=['POST'])
 def sumday():
     try:
-        result = querySelect_DB("SELECT month(log_Date) monthInYear,Day(log_Date) dayInMonth,sum(Log_kWh_Diff) as diff FROM cp_warehouse.meter_log where Month(log_Date) = MONTH(CURDATE()) and MeterID = '1' group by dayInMonth order by dayInMonth limit 30")
+        data = request.json
+
+        MeterID = data['MeterID']
+
+        result = querySelect_DB("SELECT month(log_Date) monthInYear,Day(log_Date) dayInMonth,sum(Log_kWh_Diff) as diff FROM cp_warehouse.meter_log where Month(log_Date) = MONTH(CURDATE()) and MeterID = '"+str(MeterID)+"' group by dayInMonth order by dayInMonth limit 30")
         if result == [] or result == False:
             return jsonify({"status": "fail","message":"not found"})
         # return str(result[0]['monthInYear'])
@@ -267,68 +446,7 @@ def sumday():
                 diff2 = 0
             # return str(diff)
             diff[i['dayInMonth']] = str(diff2)
-
-        # dayInMonth =[
-        #     "Jun 4",
-        #     "Jun 5",
-        #     "Jun 6",
-        #     "Jun 7",
-        #     "Jun 8",
-        #     "Jun 9",
-        #     "Jun 10",
-        #     "Jun 11",
-        #     "Jun 12",
-        #     "Jun 13",
-        #     "Jun 14",
-        #     "Jun 15",
-        #     "Jun 16",
-        #     "Jun 17",
-        #     "Jun 18",
-        #     "Jun 19",
-        #     "Jun 20",
-        #     "Jun 21",
-        #     "Jun 22",
-        #     "Jun 23",
-        #     "Jun 24",
-        #     "Jun 25",
-        #     "Jun 26",
-        #     "Jun 27",
-        #     "Jun 28",
-        #     "Jun 29",
-        #     "Jun 30"
-        # ]
-        # diff = [
-        #     "1.00",
-        #     "1.00",
-        #     "1.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "25.00",
-        #     "1.00",
-        #     "1.00",
-        #     "1.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "25.00",
-        #     "1.00",
-        #     "1.00",
-        #     "1.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00",
-        #     "22.00"
-        # ]
+        month
         return jsonify({"status": "success","dayInMonth":dayInMonth,"diff":diff})
 
     except Exception as e:
@@ -337,7 +455,10 @@ def sumday():
 @app.route('/sumyear', methods=['POST'])
 def sumyear():
     try:
-        result = querySelect_DB("SELECT month(log_Date) monthInYear,sum(Log_kWh_Diff) as diff FROM cp_warehouse.meter_log where year(log_Date) = year(CURDATE()) group by monthInYear order by monthInYear;")
+        data = request.json
+
+        MeterID = data['MeterID']
+        result = querySelect_DB("SELECT month(log_Date) monthInYear,sum(Log_kWh_Diff) as diff FROM cp_warehouse.meter_log where year(log_Date) = year(CURDATE()) AND MeterID = '"+MeterID+"'group by monthInYear order by monthInYear;")
         if result == [] or result == False:
             return jsonify({"status": "fail","message":"not found"})
         # return str(result)
